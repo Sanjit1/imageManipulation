d612e916e2af9a132e080348d45ee40f550beba2
Sanjit Sarda <sanjitsarda4@gmail.com>
2019-11-10 18:44:56 -0800

    add basic tkinter

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 import matplotlib.pyplot as plotter
+import tkinter
 import os.path
 import PIL
 
-cwd = os.path.dirname(os.path.abspath(__file__))
-picture_jpg = os.path.join(cwd, 'nyan.jpg')
-picture_cat = PIL.Image.open(picture_jpg).convert('RGB')
-picture_load = picture_cat.load()
-fig, axes = plotter.subplots(1, 1)
-axes.imshow(picture_cat, interpolation='none')
-plotter.show()
+window = tkinter.Tk()
+window.title('Background Eraser')
 
 
-def on_click(event):
-    global ix, iy
-    ix, iy = event.xdata, event.ydata
-    print('x = %d, y = %d' % (ix, iy))
-    coords = [(ix, iy)]
-    color = picture_load[ix, iy]
-    if len(coords) == 2:
-        fig.canvas.mpl_disconnect(cid)
-    print(color)
+def select_file():
+    print('hi')
+    picture_jpg = tkinter.filedialog.askopenfilename(
+        filetypes=(("Pictures", "*.png;*.jpg;*.jpeg;*.PNG;*.JPG;*.JPEG;*.gif;*.GIF;*.BMP,*.bmp"),
+                   ("AlL FiLes StUPid", "*.*")))
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    picture_cat = PIL.Image.open(picture_jpg).convert('RGBA')
+    picture_load = picture_cat.load()
+    fig, axes = plotter.subplots(1, 1)
+    axes.imshow(picture_cat, interpolation='none')
+    plotter.show()
 
-    for x in range(picture_cat.size[0]):
-        for y in range(picture_cat.size[1]):
-            r = picture_load[x, y][0]
-            b = picture_load[x, y][1]
-            g = picture_load[x, y][2]
-            if color[0] + 20 > r > color[0] - 20 and color[1] + 20 > b > color[1] - 20 and color[2] + 20 > g > color[
-                2] - 20:
-                picture_load[x, y] = (255, 255, 255)
-    picture_cat.save(picture_jpg)
-    return coords
+    def on_click(event):
+        global ix, iy
+        ix, iy = event.xdata, event.ydata
+        print('x = %d, y = %d' % (ix, iy))
+        coords = [(ix, iy)]
+        color = picture_load[ix, iy]
+        if len(coords) == 2:
+            fig.canvas.mpl_disconnect(cid)
+        print(color)
+
+        for x in range(picture_cat.size[0]):
+            for y in range(picture_cat.size[1]):
+                r = picture_load[x, y][0]
+                b = picture_load[x, y][1]
+                g = picture_load[x, y][2]
+                if color[0] + 20 > r > color[0] - 20 and color[1] + 20 > b > color[1] - 20 and color[2] + 20 > g > \
+                        color[
+                            2] - 20:
+                    picture_load[x, y] = (255, 255, 255, 0)
+        picture_cat.save((picture_jpg[:-5]+".png"), 'PNG')
+        return coords
+
+    cid = fig.canvas.mpl_connect('button_press_event', on_click)
 
 
-cid = fig.canvas.mpl_connect('button_press_event', on_click)
+select_file_button = tkinter.Button(window, text='Select File', width=25, command=select_file)
+select_file_button.pack()
+
+window.mainloop()
+
